ee8a4bf8d18ce97fe1d1309218189226b117e6ba
Presson-coder <pressonzhou04@gmail.com>
2025-05-27 20:50:22 +0200

    styling, added bg img

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,16 @@ import Projects from "./projects/page";
 
 export default function Index() {
   return (
-    <div className="bg-gradient-to-b from-gray-900 to-gray-800 px-20">
-      <div className="min-h-screen text-white flex flex-col items-center justify-center">
-        <Home />
+    <div className="">
+      <div className="min-h-screen text-white flex flex-col items-center justify-center bg-[url('/homeHero.jpg')] bg-cover bg-center bg-no-repeat">
+        <div className="absolute inset-0 bg-black/60 z-0" />
+        <div className="relative min-h-screen text-white flex flex-col items-center justify-center z-10">
+          <Home />
+        </div>
       </div>
-       <div className="min-h-screen pb-20">
+      <div className="min-h-screen py-20 bg-gradient-to-b from-gray-100 to-gray-50 px-20">
         <Projects />
-       </div>
+      </div>
     </div>
   );
 }
